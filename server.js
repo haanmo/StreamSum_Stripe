12601b5b0502fe6e4759bebaa38642ad8a71fabf
Haan Mo Johng <haanmo@naver.com>
2021-04-16 17:33:07 -0500

    new files added

---
+++ server.js
@@ -0,0 +1,14 @@
+// 1. init node js express
+var express = require('express');
+var app = express();
+
+// 2. init middlewares
+app.use(express.json());
+
+// 3. init router
+app.use('/transfer', require('./Transfer.js').transferWrapper());
+
+// 4. start a server
+app.listen(52273, function () {
+  console.log('Server running at http://127.0.0.1:52273');
+});
